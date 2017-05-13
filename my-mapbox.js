ec5ec3bcea745461be885e0ef9c96e6b0d610870
Charlie Ward <charlie@outstation.com.au>
2017-05-13 18:23:10 +1000

    first commit

---
+++ my-mapbox.js
@@ -0,0 +1,40 @@
+// Map via Mapbox GL
+
+var mapCoordinates = [42.885441,-78.878464];
+var mapZoom = 11;
+
+// the key from the Mapbox examples (not mine)
+var mapAccessToken = "pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3YmZtdjdsMSJ9.2vDbTw3ysscpy3YWkHo6aA";
+
+var map = null;
+var geocoder = null;
+
+function initMap() {
+  map = MapGL();
+}
+
+function MapGL() {
+  mapboxgl.accessToken = mapAccessToken;
+
+  // initialize map
+  var newMap = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v9", //stylesheet location
+      center: [-77.04, 38.907], // starting position
+      zoom: 11 // starting zoom
+  });
+
+  // geocoding
+  newMap.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+  }));
+  
+  // event handlers
+  newMap.on("load", mapLoaded);
+    return newMap;
+}
+
+function mapLoaded() {
+  // do stuff here
+  
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Intro
+
+This repo shows `Mapbox GL JS` and `Bootstrap` playing nicely together.
+
+This repo can be used as a kind of boilerplate for new `Mapbox / Bootstrap` projects.
+
+# Beg, borrow, steal
+
+This project borrows heavily - read steals from - this `Codepen`: [http://codepen.io/technoloG/pen/vKZNao](http://codepen.io/technoloG/pen/vKZNao)
